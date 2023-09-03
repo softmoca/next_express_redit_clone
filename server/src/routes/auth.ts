2cdb9a2e3de511a058b83fee9a639b2397df38ba
softmoca <acyc0909@gmail.com>
2023-09-03 22:38:52 +0900

    feat : logout

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -103,9 +103,23 @@ const login = async (req: Request, res: Response) => {
   }
 };
 
+const logout = async (_: Request, res: Response) => {
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0),
+      path: "/",
+    })
+  );
+  res.status(200).json({ success: true });
+};
+
 const router = Router();
 router.get("/me", userMiddleware, authMiddleware, me);
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/logout", userMiddleware, authMiddleware, logout);
 export default router;
