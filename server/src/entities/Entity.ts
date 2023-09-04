86069492a13c513d155ce171f063a330443b9d59
softmoca <acyc0909@gmail.com>
2023-09-04 19:05:50 +0900

    feat : image_upload

--- server/src/entities/Entity.ts
+++ server/src/entities/Entity.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from "class-transformer";
 import {
   BaseEntity,
   CreateDateColumn,
@@ -14,4 +15,7 @@ export default abstract class Entity extends BaseEntity {
 
   @UpdateDateColumn()
   updatedAt: Date;
+  toJSON() {
+    return instanceToPlain(this);
+  }
 }
--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -6,6 +6,35 @@ import { isEmpty } from "class-validator";
 import { AppDataSource } from "../data-source";
 import Sub from "../entities/Sub";
 import Post from "../entities/Post";
+import multer, { FileFilterCallback } from "multer";
+import { makeId } from "../utils/helpers";
+import path from "path";
+import { fstat, unlinkSync } from "fs";
+
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+  try {
+    const sub = await Sub.findOneByOrFail({ name });
+
+    // 포스트를 생성한 후에 해당 sub에 속하는 포스트 정보들을 넣어주기
+    // const posts = await Post.find({
+    //   where: { subName: sub.name },
+    //   order: { createdAt: "DESC" },
+    //   relations: ["comments", "votes"],
+    // });
+
+    // sub.posts = posts;
+
+    // if (res.locals.user) {
+    //   sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+    // }
+
+    return res.json(sub);
+  } catch (error) {
+    return res.status(404).json({ error: "커뮤니티를 찾을 수 없습니다." });
+  }
+};
+
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
 
@@ -65,8 +94,100 @@ const topSubs = async (req: Request, res: Response) => {
   }
 };
 
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  try {
+    const sub = await Sub.findOneOrFail({ where: { name: req.params.name } });
+
+    if (sub.username !== user.username) {
+      return res
+        .status(403)
+        .json({ error: "이 커뮤니티를 소유하고 있지 않습니다." });
+    }
+
+    res.locals.sub = sub;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: " 문제가 발생했습니다." });
+  }
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "public/images",
+    filename: (_, file, callback) => {
+      const name = makeId(10);
+      callback(null, name + path.extname(file.originalname));
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      callback(null, true);
+    } else {
+      callback(new Error("이미지가 아닙니다."));
+    }
+  },
+});
+
+const uploadSubImage = async (req: Request, res: Response) => {
+  const sub: Sub = res.locals.sub;
+  try {
+    const type = req.body.type;
+    // 파일 유형을 지정치 않았을 시에는 업로든 된 파일 삭제
+    if (type !== "image" && type !== "banner") {
+      if (!req.file?.path) {
+        return res.status(400).json({ error: "유효하지 않은 파일" });
+      }
+
+      // 파일을 지워주기
+      unlinkSync(req.file.path);
+      return res.status(400).json({ error: "잘못된 유형" });
+    }
+
+    let oldImageUrn: string = "";
+
+    if (type === "image") {
+      // 사용중인 Urn 을 저장합니다. (이전 파일을 아래서 삭제하기 위해서)
+      oldImageUrn = sub.imageUrn || "";
+      // 새로운 파일 이름을 Urn 으로 넣어줍니다.
+      sub.imageUrn = req.file?.filename || "";
+    } else if (type === "banner") {
+      oldImageUrn = sub.bannerUrn || "";
+      sub.bannerUrn = req.file?.filename || "";
+    }
+    await sub.save();
+
+    // 사용하지 않는 이미지 파일 삭제
+    if (oldImageUrn !== "") {
+      const fullFilename = path.resolve(
+        process.cwd(),
+        "public",
+        "images",
+        oldImageUrn
+      );
+      unlinkSync(fullFilename);
+    }
+
+    return res.json(sub);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
+};
+
 const router = Router();
 
+router.get(":/name", userMiddleware, getSub);
 router.post("/", userMiddleware, authMiddleware, createSub);
 router.get("/sub/topSubs", topSubs);
+router.post(
+  "/:name/upload",
+  userMiddleware,
+  authMiddleware,
+  ownSub,
+  upload.single("file"),
+  uploadSubImage
+);
+
 export default router;
